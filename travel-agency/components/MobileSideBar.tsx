787a379d0adb8e21e0a6189e08add27a2852c151
Aymen Hmani <aymen.hmani.tech@gmail.com>
2025-06-20 14:17:18 +0100

    Refactor MobileSideBar component; improve structure and add NavItems integration

--- travel-agency/components/MobileSideBar.tsx
+++ travel-agency/components/MobileSideBar.tsx
@@ -1,6 +1,37 @@
+// @ts-nocheck
+import {Link} from "react-router";
+import {Sidebar, SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import NavItems from "./NavItems";
+
 const MobileSideBar = () => {
+    let sidebar: SidebarComponent;
+
     return (
-        <div>MobileSideBar</div>
-    )
-}
-export default MobileSideBar
+        <div className="mobile-sidebar wrapper">
+            <header>
+                <Link to="/">
+                    <img
+                        src="/assets/icons/logo.svg"
+                        alt="logo"
+                        className="size-[30px]"
+                    />
+                    <h1>Tabaani</h1>
+                </Link>
+                <button onClick={() => sidebar.toggle()}>
+                    <img src="/assets/icons/menu.svg" alt="menu" className="size-7"/>
+                </button>
+            </header>
+            <SidebarComponent
+                width={270}
+                ref={(Sidebar) => (Sidebar = sidebar)}
+                created={() => sidebar.hide()}
+                closeOnDocumentClick={true}
+                showBackdrop={true}
+                type="over"
+            >
+                <NavItems/>
+            </SidebarComponent>
+        </div>
+    );
+};
+export default MobileSideBar;
