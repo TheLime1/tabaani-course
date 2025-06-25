8f006c343a95081a9cf173da34038fba8f2d1737
Aymen Hmani <aymen.hmani.tech@gmail.com>
2025-06-25 10:26:53 +0100

    Add StatsCard and TripCard components to dashboard
    Introduced new StatsCard and TripCard components and integrated them into the admin dashboard. Updated the components index to export the new components for easier imports.

--- travel-agency/app/routes/admin/dashboard.tsx
+++ travel-agency/app/routes/admin/dashboard.tsx
@@ -1,17 +1,29 @@
 import Header from "../../../components/Header";
+import TripCard from "../../../components/TripCard";
+import StatsCard from "../../../components/StatsCard";
 
 const dashboard = () => {
-    const user = {
-        name: "Aymen Hmani",
-    }
-    return (
-        <main className="dashboard wrapper">
-            <Header
-                title={`Welcome ${user?.name ?? "Guest"}`}
-                description="Here you can manage your travel agency, view bookings, and more."
-            />
-        </main>
-    )
-}
+  const user = {
+    name: "Aymen Hmani",
+  };
 
-export default dashboard
+  const dashboardStats = {
+    totalUsers: 100,
+    usersJoined: { currentMonth: 10, lastMonth: 8 },
+    totalTrips: 50,
+    tripsCreated: { currentMonth: 5, lastMonth: 3 },
+    userRoles: { total: 60, currentMonth: 6, lastMonth: 4 },
+  };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"}`}
+        description="Here you can manage your travel agency, view bookings, and more."
+      />
+      <StatsCard />
+      <TripCard />
+    </main>
+  );
+};
+
+export default dashboard;
